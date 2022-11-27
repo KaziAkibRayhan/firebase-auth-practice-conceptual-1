00fec58ba64eb621b3295fd405716609543ea5c4
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-27 20:40:54 +0600

    Sign up  And Sign in by firebase init

---
+++ src/Hook/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5uamffQ1lN0CiXziazz5OszbqrvvlPMk",
+  authDomain: "fir-auth-c19d7.firebaseapp.com",
+  projectId: "fir-auth-c19d7",
+  storageBucket: "fir-auth-c19d7.appspot.com",
+  messagingSenderId: "920329784183",
+  appId: "1:920329784183:web:2618b8b15aeac6717d2f72",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -2,6 +2,47 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Registration = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleName = (e) => {
+    console.log(e.target.value);
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    const emailValid = /\S+@\S+\.\S+/.test(e.target.value);
+    if (!emailValid) {
+      setError("please enter valid email address");
+      return;
+    }
+    console.log(e.target.value);
+    setEmail(e.target.value);
+    setError("");
+  };
+  const handlePassword = (e) => {
+    console.log(e.target.value);
+    if (!/(?=.{8,})/.test(e.target.value)) {
+      setError("password must be at least 8 characters long");
+      return;
+    }
+
+    if (!/(?=.*[a-zA-Z])/.test(e.target.value)) {
+      setError("password should have one uppercase");
+      return;
+    }
+    if (!/(?=.*[!#$@%&? "])/.test(e.target.value)) {
+      setError("password should have one Special character");
+      return;
+    }
+
+    setError("");
+    setPassword(e.target.value);
+  };
+
+  const handleRegister = (e) => {};
+
   return (
     <div className="mt-5">
       <div className="main-container d-flex container justify-content-between align-items-center">
@@ -14,21 +55,24 @@ const Registration = () => {
         </div>
         <div className="register-form  w-100">
           <div className="input-box">
-            <p className="text-danger">{"error"}</p>
+            <p className="text-danger">{error}</p>
             <form action="">
               <input
+                onBlur={handleName}
                 className="form-control p-3 m-2"
                 type="text"
                 placeholder="Enter your name"
                 required
               />
               <input
+                onBlur={handleEmail}
                 className="form-control p-3 m-2"
                 type="email"
                 placeholder="Email"
                 required
               />
               <input
+                onBlur={handlePassword}
                 className="form-control p-3 m-2"
                 type="password"
                 placeholder="password"
@@ -45,6 +89,7 @@ const Registration = () => {
               <span className="mb-3">accept term & condition</span>
               <br />
               <button
+                onClick={handleRegister}
                 type="submit"
                 className="btn btn-info p-3 w-50 mt-3 fw-bold text-white"
               >
